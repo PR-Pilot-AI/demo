f017c8183f58821154b6c032bca0631c22192119
Marco Lamina <mlamina09@gmail.com>
2024-06-20 11:42:23 -0700

    Improve gol scripts

--- game-of-life/demo_script_1.sh
+++ game-of-life/demo_script_1.sh
@@ -1,9 +1,14 @@
 set -e
 #!/usr/bin/env bash
 
+# Implement game of life in new PR
 pilot --sync --verbose task "Create game-of-life/gol.py, which plays a random game of life every time it is run."
+# Create dependency file
 pilot --sync --verbose task "Identify dependencies in game-of-life/gol.py, find their latest versions and create a pyproject.toml file next to it."
-pilot --sync --verbose task -f verify.md.jinja2
+# Run this command as long as poetry lock is not working
+while ! poetry lock; do
+  pilot --sync --verbose task -f verify.md.jinja2
+done
 poetry lock
 poetry install
-python gol.py
+poetry run python gol.py
